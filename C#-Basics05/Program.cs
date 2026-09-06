108324fff52cebef6581ba35b447307ff3e6965d
mahmoud <10812023100221@stud.cu.edu.eg>
2026-09-07 01:24:24 +0300

    Add project files.

---
+++ C#-Basics05/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace C__Basics05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ques 1
+
+            #endregion
+
+            #region Ques 2
+
+            #endregion
+
+            #region Ques 3
+
+            #endregion
+
+            #region Ques 4
+
+            #endregion
+
+            #region Ques 5 
+
+            #endregion
+
+            #region Ques 6
+
+            #endregion
+
+            #region Ques 7
+
+            #endregion
+
+            #region Ques 8 
+
+            #endregion
+
+        }
+    }
+}
